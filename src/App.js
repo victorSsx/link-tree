9f5e333e75bb08b64a5beca42199bb976369b7f2
victorSsx <vs469963@gmail.com>
2023-10-10 16:29:00 -0300

    linktree_v2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import react from "react";
 import './App.css';
 
-import musicSound from './sound/The Scientist com pássaros e trovões - Coldplay .mp3'
+import musicSound from './sound/The Scientist com pássaros e trovões - Coldplay .mp3';
+import imgProfile from './image/IMG_20230707_223036_915.jpg';
 
 function App() {
   return (
@@ -14,13 +15,14 @@ function App() {
         </div>
 
     <div className="linktree">
+      <img className="img" src={imgProfile} />
       <h1>Meus Links</h1>
       <ul>
-        <li><a href="https://victorssx.github.io/My_Page/">My_Page</a></li>
-        <li><a href="https://victorssx.github.io/One-Piece-X/">One Piece</a></li>
-        <li><a href="https://victorssx.github.io/Projeto-X/">X-Men</a></li>
-        <li><a href="https://victorssx.github.io/clone-netflix/">Netflix-Clone</a></li>
-        <li><a href="https://victorssx.github.io/projeto-Landing-page-pepsi-/">Landing-page pepsi</a></li>
+       <li className="b"><a href="https://victorssx.github.io/My_Page/">My_Page</a></li>
+        <li className="b"><a href="https://victorssx.github.io/One-Piece-X/">One Piece</a></li>
+        <li className="b"><a href="https://victorssx.github.io/Projeto-X/">X-Men</a></li>
+        <li className="b"><a href="https://victorssx.github.io/clone-netflix/">Netflix-Clone</a></li>
+        <li className="b"><a href="https://victorssx.github.io/projeto-Landing-page-pepsi-/">Landing-page pepsi</a></li>
       </ul>
     </div>
     </div>
