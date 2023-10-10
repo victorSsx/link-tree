fa68d2d24231810442c2473d814b2b3fec9b04aa
victorSsx <vs469963@gmail.com>
2023-10-09 22:38:05 -0300

    linktree

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import react from "react";
 import './App.css';
+
+import musicSound from './sound/The Scientist com pássaros e trovões - Coldplay .mp3'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="background"></div>
+    <div className="sumir">
+      <audio controls autoPlay>
+        <source src={musicSound} />
+        </audio>
+        </div>
+
+    <div className="linktree">
+      <h1>Meus Links</h1>
+      <ul>
+        <li><a href="https://victorssx.github.io/My_Page/">My_Page</a></li>
+        <li><a href="https://victorssx.github.io/One-Piece-X/">One Piece</a></li>
+        <li><a href="https://victorssx.github.io/Projeto-X/">X-Men</a></li>
+        <li><a href="https://victorssx.github.io/clone-netflix/">Netflix-Clone</a></li>
+        <li><a href="https://victorssx.github.io/projeto-Landing-page-pepsi-/">Landing-page pepsi</a></li>
+      </ul>
+    </div>
     </div>
   );
 }
